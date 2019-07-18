bd0403ce4978ccaf03a7d1915eef8a588103ea2d
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-07-18 16:13:50 -0500

    organize data, scripts, results in projects !

---
+++ R4DS/Chapter 08 workflow - projects.R
@@ -0,0 +1,17 @@
+# Chapter 08 workflow - projects
+getwd()
+
+# In Windows they start with a drive letter (e.g. C:) or two backslashes (e.g. \\servername) 
+# In summary, RStudio projects give you a solid workflow that will serve you well in the future:
+#   
+# -Create an RStudio project for each data analysis project.
+# 
+# -Keep data files there; we’ll talk about loading them into R in data import.
+# 
+# -Keep scripts there; edit them, run them in bits or as a whole.
+# 
+# -Save your outputs (plots and cleaned data) there.
+# 
+# -Only ever use relative paths, not absolute paths.
+# 
+# Everything you need is in one place, and cleanly separated from all the other projects that you are working on.
