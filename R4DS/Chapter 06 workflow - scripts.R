87bdeac02912787dc8f6c042ed2c252a5d671007
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-07-17 21:47:40 -0500

    run R scripts

---
+++ R4DS/Chapter 06 workflow - scripts.R
@@ -0,0 +1,10 @@
+# Chapter 6 workflow - scripts
+library(dplyr)
+library(nycflights13)
+
+not_cancelled <- flights %>%
+  filter(!is.na(dep_delay), !is.na(arr_delay))
+
+not_cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarise(mean = mean(dep_delay))
