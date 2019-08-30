07db7d86701a8a468dca3f24749fc6c6ff5a43e6
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-08-30 17:54:31 -0500

    Homogeneity of Residuals

--- PSU_R/Leson 11 Linear Regression.R
+++ PSU_R/Leson 11 Linear Regression.R
@@ -30,3 +30,23 @@ dev.off()
 par(mfrow=c(1,2))
 plot(density(model1$residuals), main = "model1")
 plot(density(model2$residuals), main = "model2")
+
+
+# 11.3 - Meeting Regression Assumptions - Homogeneity of Residuals --------
+
+Weights <- read.csv("Data/WeightData.csv", comm="#")
+hist(Weights$age, main = "")
+plot(weight ~ age, data = Weights)
+par(mfrow=c(1, 2))
+plot(log(weight) ~ age, data = Weights)
+abline(v=7, lty=2)
+
+plot(log(weight) ~ age, data = subset(Weights, age > 7))
+
+m1 <- lm(weight~age, data = Weights)
+m2 <- lm(log(weight) ~ age, data = subset(Weights, age > 7))
+par(mfrow=c(2, 2))
+plot(m1$fitted.values, m1$residuals)
+plot(density(m1$residuals))
+plot(m2$fitted.values, m2$residuals)
+plot(density(m2$residuals))
