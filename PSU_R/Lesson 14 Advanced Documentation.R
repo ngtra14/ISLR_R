7422fa94655a502375a41f42497c9fbce87ed2a8
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-09-10 23:13:13 -0500

    comments only

---
+++ PSU_R/Lesson 14 Advanced Documentation.R
@@ -0,0 +1,19 @@
+# Chapter 14: Advanced Documentation
+
+
+# 14.1 - Why You Might Want to Use R Markdown -----------------------------
+
+# R Markdown
+
+# 14.2 - Basic Features of R Markdown -------------------------------------
+
+## header block
+# ---
+# ---
+
+# **Knit**
+
+
+# 14.3 - More Features in R Markdown --------------------------------------
+
+
