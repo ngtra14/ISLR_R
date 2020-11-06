7bb6a7aadb1f1faa96037f9fe5120fade84f7e8e
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-11-06 10:47:28 -0500

    caret package documentation updated in 2019
    https://topepo.github.io/caret/index.html

---
+++ caretBook/Chapter 01 Introduction.R
@@ -0,0 +1,6 @@
+# 1 Introduction ----------------------------------------------------------
+# just a place holder for links below
+
+# https://topepo.github.io/caret/index.html
+
+# http://appliedpredictivemodeling.com/
