cef52a452a297d9da635eb32f93aa2b69b5d4177
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-11-16 21:12:36 -0500

    data sets and package info
    https://topepo.github.io/caret/session-information.html

---
+++ caretBook/Chapter 23 Data Sets.R
@@ -0,0 +1,21 @@
+# 23 Data Sets ------------------------------------------------------------
+# 23.1 Blood-Brain Barrier Data
+# 23.2 COX-2 Activity Data
+# 23.3 DHFR Inhibition
+# 23.4 Tecator NIR Data
+# 23.5 Fatty Acid Composition Data
+# 23.6 German Credit Data
+# 23.7 Kelly Blue Book
+# 23.8 Cell Body Segmentation Data
+# 23.9 Sacramento House Price Data
+# 23.10 Animal Scat Data
+
+
+# Library for data --------------------------------------------------------
+
+# caret
+# mlbench
+# SMCRM
+# AppliedPredictiveModeling
+# QSARdata
+
---
+++ caretBook/Chapter 24 Session Information.R
@@ -0,0 +1,4 @@
+# 24 Session Information --------------------------------------------------
+# https://topepo.github.io/caret/session-information.html
+# place holder for info
+
